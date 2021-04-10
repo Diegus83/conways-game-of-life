ec2cb92b5f8e6cc85351581eadaff46f6b87fb3b
Diegus83 <diegus83@gmail.com>
2021-04-10 16:24:30 -0400

    Start/pause and clear button implemented

--- model.py
+++ model.py
@@ -3,20 +3,19 @@ import random
 HEIGHT = 100
 WIDTH = 100
 
+
 def randomize(grid, width, height):
     for i in range(0, height):
         for j in range(0, width):
-            grid[i][j] = random.randint(0,1)
+            grid[i][j] = random.randint(0, 1)
+
 
 GRID_MODEL = [0] * HEIGHT
 NEXT_GRID_MODEL = [0] * HEIGHT
-
-
 for i in range(HEIGHT):
     GRID_MODEL[i] = [0] * WIDTH
     NEXT_GRID_MODEL[i] = [0] * WIDTH
 
-randomize(GRID_MODEL, WIDTH, HEIGHT)
 
 def next_gen():
     global GRID_MODEL, NEXT_GRID_MODEL
@@ -24,7 +23,7 @@ def next_gen():
     for i in range(0, HEIGHT):
         for j in range(0, WIDTH):
             cell = 0
-            print('Checking Cell {} {}'.format(i,j))
+            print("Checking Cell {} {}".format(i, j))
             count = count_neighbors(GRID_MODEL, i, j)
 
             if GRID_MODEL[i][j] == 0:
@@ -34,30 +33,66 @@ def next_gen():
                 if count == 2 or count == 3:
                     cell = 1
             NEXT_GRID_MODEL[i][j] = cell
-            print('New value is {}'.format(NEXT_GRID_MODEL[i][j]))
+            print("New value is {}".format(NEXT_GRID_MODEL[i][j]))
 
     GRID_MODEL, NEXT_GRID_MODEL = NEXT_GRID_MODEL, GRID_MODEL
+
 
 def count_neighbors(grid, row, col):
 
     count = 0
-    if row-1 >= 0:
-        count +=  grid[row-1][col]
-    if (row-1 >= 0) and (col-1 >= 0):
-        count +=  grid[row-1][col-1]
-    if (row-1 >= 0) and (col+1 < WIDTH):
-        count +=  grid[row-1][col+1]
-    if col-1 >= 0:
-        count +=  grid[row][col-1]
+    if row - 1 >= 0:
+        count += grid[row - 1][col]
+    if (row - 1 >= 0) and (col - 1 >= 0):
+        count += grid[row - 1][col - 1]
+    if (row - 1 >= 0) and (col + 1 < WIDTH):
+        count += grid[row - 1][col + 1]
+    if col - 1 >= 0:
+        count += grid[row][col - 1]
     if col + 1 < WIDTH:
-        count +=  grid[row][col+1]
+        count += grid[row][col + 1]
     if row + 1 < HEIGHT:
-        count +=  grid[row+1][col]
-    if (row + 1 < HEIGHT) and (col-1 >= 0):
-        count +=  grid[row+1][col-1]
-    if (row + 1 < HEIGHT) and (col+1 < WIDTH):
-        count +=  grid[row+1][col+1]
+        count += grid[row + 1][col]
+    if (row + 1 < HEIGHT) and (col - 1 >= 0):
+        count += grid[row + 1][col - 1]
+    if (row + 1 < HEIGHT) and (col + 1 < WIDTH):
+        count += grid[row + 1][col + 1]
     return count
+
+glider_pattern = [[0, 0, 0, 0, 0],
+                  [0, 0, 1, 0, 0],
+                  [0, 0, 0, 1, 0],
+                  [0, 1, 1, 1, 0],
+                  [0, 0, 0, 0, 0]]
+
+
+glider_gun_pattern = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+              [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+def load_pattern(pattern, x_offset=0, y_offset=0):
+    global GRID_MODEL
+
+    for i in range(0, HEIGHT):
+        for j in range(0, WIDTH):
+            GRID_MODEL[i][j] = 0
+
+    j = y_offset
+
+    for row in pattern:
+        i = x_offset
+        for value in row:
+            GRID_MODEL[i][j] = value
+            i = i + 1
+        j = j + 1
 
 if __name__ == "__main__":
     next_gen()
--- view.py
+++ view.py
@@ -2,6 +2,7 @@ import tkinter
 import model
 
 CELL_SIZE = 5
+IS_RUNNING = False
 
 
 def setup():
@@ -30,11 +31,68 @@ def setup():
     option.config(width=20)
 
     grid_view.grid(row=0, columnspan=3, padx=20, pady=20)
-    start_button.grid(row=1, column=0, sticky='W', padx=20, pady=20)
+    start_button.grid(row=1, column=0, sticky='w', padx=20, pady=20)
+    start_button.bind('<Button-1>', start_handler)
     option.grid(row=1, column=1, padx=20)
-    clear_button.grid(row=1, column=2, sticky='E', padx=20, pady=20)
+    clear_button.grid(row=1, column=2, sticky='e', padx=20, pady=20)
+    clear_button.bind('<Button-1>', clear_handler)
+
+def start_handler(event):
+    global IS_RUNNING, start_button
+
+    if IS_RUNNING:
+        IS_RUNNING = False
+        start_button.configure(text='Start')
+    else:
+        IS_RUNNING = True
+        start_button.configure(text='Pause')
+        update()
+
+def clear_handler(event):
+    global IS_RUNNING, start_button
+
+    IS_RUNNING = False
+    for i in range(0, model.HEIGHT):
+        for j in range(0, model.WIDTH):
+            model.GRID_MODEL[i][j] = 0
+
+    start_button.configure(text='Start')
+    update()
+
+def update():
+    global grid_view, root, IS_RUNNING
+
+    grid_view.delete('all')
+
+    model.next_gen()
+    for i in range(0, model.HEIGHT):
+        for j in range(0, model.WIDTH):
+            if model.GRID_MODEL[i][j] == 1:
+                draw_cell(i, j, "black")
+
+    if IS_RUNNING:
+        root.after(100, update)
+
+
+def draw_cell(row, col, color):
+    global grid_view, CELL_SIZE
+    if color == "black":
+        outline = "grey"
+    else:
+        outline = "white"
+
+    grid_view.create_rectangle(
+        row * CELL_SIZE,
+        col * CELL_SIZE,
+        row * CELL_SIZE + CELL_SIZE,
+        col * CELL_SIZE + CELL_SIZE,
+        fill=color,
+        outline=outline,
+    )
 
 
 if __name__ == "__main__":
     setup()
+    model.randomize(model.GRID_MODEL, model.WIDTH, model.HEIGHT)
+    update()
     tkinter.mainloop()
